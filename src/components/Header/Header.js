bdcc554121579e0b5f1ae135bcbeadc104c98461
Graham Houser <gehouser@live.com>
2020-07-03 13:35:48 -0500

    First commit from template

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,11 +29,11 @@ export default function Header() {
             height="38"
             alt="React"
           />
-          <span className={s.brandTxt}>Your Company</span>
+          <span className={s.brandTxt}>Flip the Table</span>
         </Link>
         <div className={s.banner}>
-          <h1 className={s.bannerTitle}>React</h1>
-          <p className={s.bannerDesc}>Complex web apps made easy</p>
+          <h1 className={s.bannerTitle}>Test Name</h1>
+          <p className={s.bannerDesc}>Catchphrase I do not have yet</p>
         </div>
       </div>
     </div>
